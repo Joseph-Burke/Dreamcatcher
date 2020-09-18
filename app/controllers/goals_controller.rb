b35e1a9ac9f928611c49098109af07ba37f8d3ce
Joseph Burke <joseph_burke@outlook.com>
2020-09-18 14:12:13 +0100

    Create basic goal views. Add #show for Goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,12 +1,16 @@
 class GoalsController < ApplicationController
-  
+
   def new
     @goal = Goal.new
   end
-  
+
   def create
     @user = User.find(session[:user_id])
     @goal = @user.goals.build(goal_params)
+  end
+
+  def show
+    @goal = Goal.find(params[:goal][:id])
   end
 
   private
@@ -14,5 +18,4 @@ class GoalsController < ApplicationController
   def goal_params
     params.require(:goal).permit(:name, :description)
   end
-
 end
