beee30db504567d8bbae78572d88f43efaedf2ca
Joseph Burke <joseph_burke@outlook.com>
2020-09-18 14:59:35 +0100

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create show]
   resources :goals, only: %i[new create show]
   resources :sessions, only: %i[new create]
+  root to: 'sessions#new'
 end
