e0ef71f2ec92feefbe06f4b0a14524394144c661
Joseph Burke <joseph_burke@outlook.com>
2020-09-18 14:49:50 +0100

    Implement redirect upon logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:user_id] = User.find_by(name: params[:name])
+    @user = User.find_by(name: params[:name])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user.id)
   end
 end
